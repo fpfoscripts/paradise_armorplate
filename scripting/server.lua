cd6ed92b5db3ff8a002a36c947ba4e5cad45243f
FPFO-Scripts <111294346+fpfoscripts@users.noreply.github.com>
2025-01-28 21:32:48 +0100

    Update server.lua

--- scripting/server.lua
+++ scripting/server.lua
@@ -1,16 +1,16 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+ESX = exports["es_extended"]:getSharedObject()
 
 for plateType, config in pairs(Config.ArmorPlates) do
-    QBCore.Functions.CreateUseableItem(config.item, function(source)
+    ESX.RegisterUsableItem(config.item, function(source)
         TriggerClientEvent('paradise_armorplate:client:useArmor', source, plateType)
     end)
 end
 
 RegisterNetEvent('paradise_armorplate:server:removePlate', function(itemName)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if not Player then return end
-    
-    Player.Functions.RemoveItem(itemName, 1)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[itemName], "remove")
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if not xPlayer then return end
+
+    xPlayer.removeInventoryItem(itemName, 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, { name = itemName }, "remove")
 end)
